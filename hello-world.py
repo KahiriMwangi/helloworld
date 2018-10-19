15e7368b30a674876b9474e6c02f70737e9369f1
KahiriMwangi <44293577+KahiriMwangi@users.noreply.github.com>
2018-10-19 15:37:57 +0300

    hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+
+Kahiri Mwangi
+mr.mwangi@hotmail.com
++254 714 172 410
++254 774 996 730
+
+Fri Oct 19 15:35:08 2018
+
+'''
+
+def hello():
+    print('Hello World')
+    
+if __name__ == '__main__': 
+    hello()
